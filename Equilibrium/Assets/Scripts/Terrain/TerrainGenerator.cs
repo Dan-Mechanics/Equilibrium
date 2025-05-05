f4b0c679d1b07509386ca47d72b277475f46a7df
Dan-Mechanics <github.tim@desmidt.org>
2025-05-05 14:03:31 +0200

    W

--- Equilibrium/Assets/Scripts/Terrain/TerrainGenerator.cs
+++ Equilibrium/Assets/Scripts/Terrain/TerrainGenerator.cs
@@ -37,6 +37,15 @@ namespace OuterWilds
             GenerateStarterTerrain();
         }
 
+        /// <summary>
+        /// I know this breaks solid but its for debug...
+        /// </summary>
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                GenerateStarterTerrain();
+        }
+
         [ContextMenu(nameof(GenerateStarterTerrain))]
         private void GenerateStarterTerrain() 
         {
