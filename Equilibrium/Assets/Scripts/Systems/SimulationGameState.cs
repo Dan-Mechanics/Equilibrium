b0e23b97a59f5562ceae9657d477a5674cac747d
Dan-Mechanics <github.tim@desmidt.org>
2025-05-15 17:30:47 +0200

    Update SimulationGameState.cs

--- Equilibrium/Assets/Scripts/Systems/SimulationGameState.cs
+++ Equilibrium/Assets/Scripts/Systems/SimulationGameState.cs
@@ -1,6 +1,4 @@
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine.Events;
 
 namespace Equilibrium
@@ -12,11 +10,7 @@ namespace Equilibrium
         [SerializeField] private float hyperSpeedScale = default;
         [SerializeField] private float simulationTime = default;
 
-      //  [SerializeField] private ButtonSwapper playSwapper = default;
-        /*[SerializeField] private BetterButton waterMountainButton = default; 
-        [SerializeField] private BetterButton wipeCleanButton = default;*/
-
-        [SerializeField] private UnityEvent<string> onNewTimerText = default;
+        [SerializeField] private UnityEvent<float> onNewTimerText = default;
 
         private readonly Timer timer = new();
 
@@ -24,16 +18,9 @@ namespace Equilibrium
         {
             base.EnterState();
 
-            /*playButton.SetActive(false);
-            stopButton.SetActive(!playButton.activeSelf);*/
-           // playSwapper.SetAs(false);
-            //playSwapper.BetterButton.GiveCooldown(0.5f);
             creatureHandler.Respawn();
             timeHandler.SetTimeScale(hyperSpeedScale);
             timer.SetValue(simulationTime);
-
-          //  waterMountainButton.SetInteractable(false);
-          //  wipeCleanButton.SetInteractable(false);
 
             EventManager.RaiseEvent(EventManager.EventType.ROUND_START);
         }
@@ -48,7 +35,7 @@ namespace Equilibrium
                 return;
             }
 
-            onNewTimerText?.Invoke(Mathf.Round(timer.Value).ToString());
+            onNewTimerText?.Invoke(Mathf.Round(timer.Value));
         }
 
         public override void ExitState()
@@ -56,13 +43,9 @@ namespace Equilibrium
             base.ExitState();
 
             print(timer.Value);
-            onNewTimerText?.Invoke(string.Empty);
 
-         //   playSwapper.SetAs(true);
-          //  playSwapper.BetterButton.GiveCooldown(0.5f);
-
-         //   waterMountainButton.SetInteractable(true);
-         //   wipeCleanButton.SetInteractable(true);
+            // ??
+            onNewTimerText?.Invoke(0f);
 
             timeHandler.BackToNormal();
             creatureHandler.Stop();
