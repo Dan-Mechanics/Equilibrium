76178ded122ee4a9a35c8f9bd5c1f6abde190172
Dan-Mechanics <github.tim@desmidt.org>
2025-05-19 15:54:19 +0200

    W

--- Equilibrium/Assets/Scripts/Camera/Zoomer.cs
+++ Equilibrium/Assets/Scripts/Camera/Zoomer.cs
@@ -1,6 +1,4 @@
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 
 namespace Equilibrium
 {
@@ -10,19 +8,13 @@ namespace Equilibrium
         [SerializeField] private float minFov = default;
         [SerializeField] private float maxFov = default;
         [SerializeField] private float zoomSens = default;
-
         [SerializeField] private float fov = default;
-
-        private void Start()
-        {
-            RenderSettings.fog = !cam.orthographic;
-        }
 
         private void Update()
         {
             fov += Input.mouseScrollDelta.y * -zoomSens;
             fov = Mathf.Clamp(fov, minFov, maxFov);
-             
+            
             if (cam.orthographic)
             {
                 cam.orthographicSize = fov;
