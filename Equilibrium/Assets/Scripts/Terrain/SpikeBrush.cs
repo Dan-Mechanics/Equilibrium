ae8911d8c8af3d01e3943e5f351ad633b6632fe8
Dan-Mechanics <github.tim@desmidt.org>
2025-05-29 16:19:21 +0200

    wdwd

--- Equilibrium/Assets/Scripts/Terrain/SpikeBrush.cs
+++ Equilibrium/Assets/Scripts/Terrain/SpikeBrush.cs
@@ -2,16 +2,27 @@ using UnityEngine;
 
 namespace Equilibrium
 {
-    [CreateAssetMenu(menuName = "ScriptableObject/" + nameof(SpikeBrush), fileName = "New " + nameof(SpikeBrush))]
-    public class SpikeBrush : BaseBrush
+    [CreateAssetMenu(menuName = "ScriptableObject/" + nameof(MixedBrush), fileName = "New " + nameof(MixedBrush))]
+    public class MixedBrush : BaseBrush
     {
-        public override float GetBrushMod(float dist, float brushSize)
+        /*public override float GetBrushMod(float dist, float brushSize)
         {
             float result = 1f;
             if (dist < 3f)
                 result += 0.3f;
 
-            return 1f + result;
+            return (1f + result + _GetBrushMod(dist, brushSize)) / 2f;
+        }*/
+
+        public override float GetBrushMod(float dist, float brushSize)
+        {
+            dist = 1f - (dist / Utils.Root(brushSize));
+            dist *= 1.32f;
+
+            if (dist < 3f)
+                dist += 0.6f;
+
+            return dist;
         }
     }
 }
