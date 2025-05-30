6c4b075026e4dc84a6e6a4852eb8237fb04c0fc9
Dan-Mechanics <github.tim@desmidt.org>
2025-05-30 14:03:36 +0200

    working on debugs

--- Equilibrium/Assets/Scripts/Systems/TitleHandler.cs
+++ Equilibrium/Assets/Scripts/Systems/TitleHandler.cs
@@ -25,6 +25,11 @@ namespace Equilibrium
         private TitleMessage current;
         private Color fadingColor;
         private readonly Dictionary<EventManager.EventType, TitleMessage> conversions = new();
+
+        /// <summary>
+        /// Consider making this just one method considering that you always have
+        /// one qqueueud up, then it needs to be a class too.
+        /// </summary>
         private readonly Queue<TitleMessage> pendingTitleMessages = new();
 
         private void FixedUpdate()
@@ -53,8 +58,6 @@ namespace Equilibrium
 
         private void EnqueueTitle(TitleMessage message) 
         {
-            //print(message);
-            
             if (message.duration <= 0f)
                 message.duration = introductionTime;
 
@@ -109,12 +112,12 @@ namespace Equilibrium
             fadingColor = current.color;
             title.color = fadingColor;
 
-            print(current.message);
+            print($"{current.message} ...");
         }
 
         public void SetTitleWithMessage(string message) 
         {
-            print(message);
+            print($"queueing {message} ...");
             standard.message = message;
             EnqueueTitle(standard);
         }
--- Equilibrium/Assets/Scripts/Terrain/SpikeBrush.cs
+++ Equilibrium/Assets/Scripts/Terrain/SpikeBrush.cs
@@ -5,15 +5,6 @@ namespace Equilibrium
     [CreateAssetMenu(menuName = "ScriptableObject/" + nameof(MixedBrush), fileName = "New " + nameof(MixedBrush))]
     public class MixedBrush : BaseBrush
     {
-        /*public override float GetBrushMod(float dist, float brushSize)
-        {
-            float result = 1f;
-            if (dist < 3f)
-                result += 0.3f;
-
-            return (1f + result + _GetBrushMod(dist, brushSize)) / 2f;
-        }*/
-
         public override float GetBrushMod(float dist, float brushSize)
         {
             dist = 1f - (dist / Utils.Root(brushSize));
